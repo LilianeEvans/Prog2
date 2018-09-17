99f8a2a3db921564b66e35e3d4cff0f3b2335a21
Kovács Nikolett <36476171+LilianeEvans@users.noreply.github.com>
2018-09-17 18:30:25 +0200

    Update Yoda.java

--- Első hét/Yoda.java
+++ Első hét/Yoda.java
@@ -1,7 +1,9 @@
-public class Yoda{
-
-public static void main(String[]args){
-String myString = null;
-if (myString.equals("nemnyert")) {System.out.println("Nem nyert.");}
-}
+public class Yoda
+{
+   public static void main(String[]args)
+    {
+    String myString = null;
+    if (myString.equals("nemnyert")) 
+      {System.out.println("Nem nyert.");}
+    }
 }
